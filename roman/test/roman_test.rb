0dacdbda139bfbced24da7a1017e0ac22be69240
usha <ushaambig@railsfactory.org>
2014-07-04 10:34:15 +0530

    corrected the test for 799

--- roman/test/roman_test.rb
+++ roman/test/roman_test.rb
@@ -20,7 +20,7 @@ class TestRoman < Test::Unit::TestCase
     assert_equal 'LXXXVII' ,Roman.convert(87)
     assert_equal 'CXXV'  , Roman.convert(125)
     assert_equal 'CCCLXXXXVII'  , Roman.convert(397)
-    assert_equal 'DCCVLXXXXIX'  , Roman.convert(799)
+    assert_equal 'DCCLXXXXIX'  , Roman.convert(799)
       
   end
 
