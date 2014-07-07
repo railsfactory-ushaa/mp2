6a3becb582c83df8071962e3f3c3e631d6a1bc67
usha <ushaambig@railsfactory.org>
2014-07-07 10:56:13 +0530

    wrote a test case for 33 and made it pass

--- spell_digit/test/spell_digit_test.rb
+++ spell_digit/test/spell_digit_test.rb
@@ -13,6 +13,7 @@ class TestSpell_digit < Test::Unit::TestCase
    assert_equal "ten",Spell_digit.convert(10)
    assert_equal "eleven",Spell_digit.convert(11)
    assert_equal "twelve",Spell_digit.convert(12)
+   assert_equal "thirty three",Spell_digit.convert(33)
    
    
   end
