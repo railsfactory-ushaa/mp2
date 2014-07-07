802441fc7336450d96a066167ca19cc01187b1fb
usha <ushaambig@railsfactory.org>
2014-07-07 12:35:27 +0530

    wrote a test case for 2and made it pass

--- numbers/lib/numbers.rb
+++ numbers/lib/numbers.rb
@@ -1,10 +1,19 @@
 class Numbers
 
   def self.convert(number)
-    if number == 1||21|31
+    a1 = [1,21,31]
+    a2 = [2,22]
+    a3 = [3,23]
+    a4 = [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,24,25,26,27,28,29,30]
+    if a1.include?(number)
     return number.to_s + "st"
-    
+    elsif a2.include?(number)
+    return number.to_s+ "nd"
+    elsif a3.include?(number)
+    return number.to_s + "rd"
+    else return number.to_s + "th"
     end
+    
   
   end
 
--- numbers/test/numbers_test.rb
+++ numbers/test/numbers_test.rb
@@ -8,9 +8,9 @@ class TestNumbers < Test::Unit::TestCase
 
   def test_num
     assert_equal '1st',Numbers.convert(1)
-    assert_equal '21st',Numbers.convert(21)
-    assert_equal '31st',Numbers.convert(31)
-    
+    assert_equal '2nd',Numbers.convert(2)
+     
+   
    
   end
 
