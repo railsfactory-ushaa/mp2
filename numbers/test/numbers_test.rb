fd6927b114b110afa0f67fc5853445b4a531ecb0
usha <ushaambig@railsfactory.org>
2014-07-07 12:37:56 +0530

    wrote a test case for 4 and made it pass

--- numbers/test/numbers_test.rb
+++ numbers/test/numbers_test.rb
@@ -10,6 +10,7 @@ class TestNumbers < Test::Unit::TestCase
     assert_equal '1st',Numbers.convert(1)
     assert_equal '2nd',Numbers.convert(2)
     assert_equal '3rd',Numbers.convert(3)
+    assert_equal '4th',Numbers.convert(4)
      
    
    
