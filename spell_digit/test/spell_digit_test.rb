482eb6a9b48d7c1527503c9212c57a9f869c1331
usha <ushaambig@railsfactory.org>
2014-07-07 11:17:03 +0530

    wrote a test case for 8999 and made it pass

--- spell_digit/test/spell_digit_test.rb
+++ spell_digit/test/spell_digit_test.rb
@@ -19,6 +19,7 @@ class TestSpell_digit < Test::Unit::TestCase
    assert_equal "one thousand two hundred and fifty",Spell_digit.convert(1250)
    assert_equal "two thousand three hundred and fifty",Spell_digit.convert(2350)
    assert_equal "five thousand eight hundred and ninety",Spell_digit.convert(5890)
+   assert_equal "eight thousand nine hundred and ninety nine",Spell_digit.convert(8999)
    
    
    
