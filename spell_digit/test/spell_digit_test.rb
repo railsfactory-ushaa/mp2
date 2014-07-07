826a0c6b188b1adcb3360a84c6d6085db7b15a2f
usha <ushaambig@railsfactory.org>
2014-07-07 10:49:40 +0530

    wrote a code to spell the digits of currency and wrote test case for 10 and made it pass

---
+++ spell_digit/test/spell_digit_test.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/spell_digit'
+require 'test/unit'
+
+
+class TestSpell_digit < Test::Unit::TestCase
+
+  def test_hello
+    assert true
+  end
+
+
+  def test_one
+   assert_equal "ten",Spell_digit.convert(10)
+   
+  end
+end
