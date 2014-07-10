0fbfd16a1a835b13125d4c2722c36f6c52c359e6
usha <ushaambig@railsfactory.org>
2014-07-10 19:11:23 +0530

    passed the test for adding 4 items,modifying,deleting and showing  pending and completed tasks

--- todonew/lib/todolist.rb
+++ todonew/lib/todolist.rb
@@ -1,58 +1,68 @@
 class Todolist
- attr_accessor :filename, :todo, :pending, :completed
+ attr_accessor :filename,:todo,:pending,:completed
 @pending = []
 @todo = []
 @completed = []
- def initialize(filename)
+
+ def self.initialize(filename)
    @filename = filename
  end
+
 def self.pending
   return @pending.size
 end
+
 def self.list
   @todo = @pending + @completed
  return @todo.size
 end
+
 def self.completed
    return @completed.size
 end
-def self.add(item)
-  @pending << item
- return @pending.size
+
+def self.add(title)
+  @pending << title
+ return @pending.count
 end
+
 def self.complete(num)
-s = @todo[num] +" "+ "done"
-var = @todo[num]
-@completed << var
-@pending.delete var
-return s
+@completed << @pending[num - 1]
+@pending.delete_at(num - 1)
+return @completed.size
 end
+
 #def self.incomplete(num)
 #var = @todo[num]
 #@pending << var
 #@completed.delete var
 #end
+
 def self.delete(num)
-@completed.delete_at(num)
+@completed.delete_at(num - 1)
 return @completed.size
 end
-def self.modify(num,item)
-#@pending.delete_at(num)
-@pending[num] = item 
-return @pending[num]
+
+def self.modify(num,title)
+@pending[num - 1] = title
+return @pending[num - 1]
 end
+
 def self.empty
 @pending.clear
 @completed.clear
 @todo.clear
 return true
 end
-def show_pending(num)
-return @pending[num]
+
+def self.show_pending(num)
+ return @pending[num - 1]
 end
-def show_completed(num)
-return @completed[num]
+
+def self.show_completed(num)
+ return @completed[num - 1]
 end
+
 end
 #t = Todolist.new('usha.txt')
 #t.pending
---
+++ todonew/test/todo_test.rb
@@ -0,0 +1,65 @@
+require_relative '../lib/todolist.rb'
+require 'test/unit'
+
+
+
+class TestTodo < Test::Unit::TestCase
+ def test_empty
+  assert_equal true,Todolist.empty
+  assert_equal 0,Todolist.pending
+  assert_equal 0,Todolist.completed
+  assert_equal 0,Todolist.list
+ end
+  def test_add1
+   assert_equal 1,Todolist.add("open")
+   assert_equal 1,Todolist.list
+   assert_equal 1,Todolist.pending
+   assert_equal 0,Todolist.completed
+ end
+  def test_add2
+   assert_equal 2,Todolist.add("search")
+   assert_equal 2,Todolist.list
+   assert_equal 2,Todolist.pending
+   assert_equal 0,Todolist.completed
+ end
+ def test_add3
+   assert_equal 3,Todolist.add("write")
+   assert_equal 3,Todolist.list
+   assert_equal 3,Todolist.pending
+   assert_equal 0,Todolist.completed
+ end
+ def test_add4
+   assert_equal 4,Todolist.add("save")
+   assert_equal 4,Todolist.list
+   assert_equal 4,Todolist.pending
+   assert_equal 0,Todolist.completed
+ end
+ 
+ def test_complete
+   assert_equal 1,Todolist.complete(1)
+   assert_equal 3,Todolist.pending
+   assert_equal 1,Todolist.completed
+   assert_equal 4,Todolist.list
+ end
+
+ def test_modify
+   assert_equal "merge",Todolist.modify(2,"merge")
+   assert_equal "read",Todolist.modify(1,"read")
+   assert_equal 2,Todolist.pending
+   assert_equal 0,Todolist.completed
+   assert_equal 2,Todolist.list
+ end
+ 
+ def test_delete
+   assert_equal 0,Todolist.delete(1)
+   assert_equal 3,Todolist.pending
+   assert_equal 0,Todolist.completed
+   assert_equal 3,Todolist.list
+ end
+def test_show_pending
+  assert_equal "merge",Todolist.show_pending(2)
+end
+def test_show_completed
+  assert_equal nil,Todolist.show_completed(1)
+end
+end
