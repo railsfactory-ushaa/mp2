36260656af575afcef1c6126562f91f4532b9655
usha <ushaambig@railsfactory.org>
2014-07-11 16:48:16 +0530

    passed 8 tests with 48 assertions

--- todonew/test/todo_test.rb
+++ todonew/test/todo_test.rb
@@ -16,113 +16,181 @@ def teardown
 end
 
 
- def test_zempty
+def test_zempty
   @a.empty
   assert_equal 0,@a.pending.size
   assert_equal 0,@a.completed.size
   assert_equal 0,@a.list.size
- end
+end
 
 
 
-  def test_add1
-   @a.empty
-   @a.add("one")
-   assert_equal 1,@a.list.size
-   assert_equal 1,@a.pending.size
-   assert_equal 0,@a.completed.size
- end
+def test_add1
+  # precondition
+  @a.empty
+
+  #before state
+  
+  assert_equal 0,@a.list.size
+  assert_equal 0,@a.pending.size
+  assert_equal 0,@a.completed.size
+
+  #action
+
+  @a.add("one")
+
+  #after
+
+  assert_equal 1,@a.list.size
+  assert_equal 1,@a.pending.size
+  assert_equal 0,@a.completed.size
+end
 
 
+def test_add2
+  # precondition
+  @a.empty
 
-  def test_add2
-@a.empty
-@a.add("one")
-   @a.add("two")
-   assert_equal 2,@a.list.size
-   assert_equal 2,@a.pending.size
-   assert_equal 0,@a.completed.size
- end
+  #before state
 
+  assert_equal 0,@a.list.size
+  assert_equal 0,@a.pending.size
+  assert_equal 0,@a.completed.size
+  
+  #action
 
-=begin
+  @a.add("one")
+  @a.add("two")
+ 
+  #after
 
- def test_add3
-
-   assert_equal 3,@a.add("write")
-   assert_equal 3,@a.list
-   assert_equal 3,@a.pending
-   assert_equal 0,@a.completed
- end
- def test_add4
-   assert_equal 4,@a.add("save")
-   assert_equal 4,@a.list
-   assert_equal 4,@a.pending
-   assert_equal 0,@a.completed
- end
-=end
-
+  assert_equal 2,@a.list.size
+  assert_equal 2,@a.pending.size
+  assert_equal 0,@a.completed.size
+end
 
   
- def test_complete
-# precondition
-@a.empty
-@a.add("one")
-#before state
-assert_equal 1,@a.pending.size
-assert_equal 0,@a.completed.size
-assert_equal 1,@a.list.size
+def test_complete
+  # precondition
+  @a.empty
+  @a.add("one")
 
+  #before state
 
-#action
-   @a.complete(1)
+  assert_equal 1,@a.pending.size
+  assert_equal 0,@a.completed.size
+  assert_equal 1,@a.list.size
 
-#after 
-assert_equal 0,@a.pending.size
-assert_equal 1,@a.completed.size
-assert_equal 1,@a.list.size
- end
+  #action
 
+  @a.complete(1)
+
+  #after 
+
+  assert_equal 0,@a.pending.size
+  assert_equal 1,@a.completed.size
+  assert_equal 1,@a.list.size
+end
 
 
 def test_delete
-# precondition
-@a.empty
-@a.add("one")
- @a.complete(1)
+  # precondition
+  @a.empty
+  @a.add("one")
+  @a.complete(1)
 
-#before state
-#before state
-assert_equal 0,@a.pending.size
-assert_equal 1,@a.completed.size
-assert_equal 1,@a.list.size
+  #before state
+
+  assert_equal 0,@a.pending.size
+  assert_equal 1,@a.completed.size
+  assert_equal 1,@a.list.size
+  
+  #action
 
   @a.delete(1)
 
-   assert_equal 0,@a.pending.size
-   assert_equal 0,@a.completed.size
-   assert_equal 0,@a.list.size
- end
+  #after
+
+  assert_equal 0,@a.pending.size
+  assert_equal 0,@a.completed.size
+  assert_equal 0,@a.list.size
+end
 
 
-=begin
+def test_modify
+  # precondition
+  @a.empty
+  @a.add("one")
+  @a.add("two")
 
- def test_modify
-   assert_equal "merge",@a.modify(3,"merge")
-   assert_equal "read",@a.modify(4,"read")
-   assert_equal 4,@a.pending
-   assert_equal 0,@a.completed
-   assert_equal 4,@a.list
- end
+ #before state
+
+  assert_equal 2,@a.pending.size
+  assert_equal 0,@a.completed.size
+  assert_equal 2,@a.list.size
+
+  #action
+
+  @a.modify(1,"merge")
+  @a.modify(2,"read")
+
+  #after
+
+  assert_equal 2,@a.pending.size
+  assert_equal 0,@a.completed.size
+  assert_equal 2,@a.list.size
+end
  
 
 def test_pend
-  assert_equal "write",@a.show_pending(2)
-  assert_equal "read",@a.show_pending(4)
+  # precondition
+  @a.empty
+  @a.add("one")
+  @a.add("two")
+  @a.add("three")
+
+  #before state
+
+  assert_equal 3,@a.pending.size
+  assert_equal 0,@a.completed.size
+  assert_equal 3,@a.list.size
+
+  #action
+
+  assert_equal "two",@a.show_pending(2)
+  assert_equal "three",@a.show_pending(3)
+
+  #after
+
+  assert_equal 3,@a.pending.size
+  assert_equal 0,@a.completed.size
+  assert_equal 3,@a.list.size
+
 end
+
 def test_qcompleted
-  assert_equal nil,@a.show_completed(1)
-  
+  #pre condition
+  @a.empty
+  @a.add("one")
+  @a.add("two")
+  @a.complete(1)
+
+  #before state
+
+  assert_equal 1,@a.pending.size
+  assert_equal 1,@a.completed.size
+  assert_equal 2,@a.list.size
+
+  #action
+
+  assert_equal "one",@a.show_completed(1)
+
+  #after
+
+  assert_equal 1,@a.pending.size
+  assert_equal 1,@a.completed.size
+  assert_equal 2,@a.list.size
+
 end
-=end
+
 end
