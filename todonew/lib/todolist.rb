de789ecd978dcd1008b4eda4a7b41e3e132ce010
usha <ushaambig@railsfactory.org>
2014-07-11 14:54:07 +0530

    simplified code that runs for all scenarios

--- todonew/lib/todolist.rb
+++ todonew/lib/todolist.rb
@@ -1,75 +1,75 @@
-class Todolist
- attr_accessor :filename,:todo,:pending,:completed
-@pending = []
-@todo = []
-@completed = []
-
- def self.initialize(filename)
+class Todolist 
+$pending = []
+$todo = []
+$completed = []
+ attr_accessor :filename
+ def initialize(filename)
    @filename = filename
+
  end
 
-def self.pending
-  return @pending.size
+def pending
+  return $pending.size
 end
 
-def self.list
-  @todo = @pending + @completed
- return @todo.size
+def list
+  $todo = $pending + $completed
+ return $todo.size
 end
 
-def self.completed
-   return @completed.size
+def completed
+   return $completed.size
 end
 
-def self.add(title)
-  @pending << title
- return @pending.count
+def add(title)
+  $pending << title
+ return $pending.count
 end
 
-def self.complete(num)
-@completed << @pending[num - 1]
-@pending.delete_at(num - 1)
-return @completed.size
+def complete(num)
+$completed << $pending[num - 1]
+$pending.delete_at(num - 1)
+return $completed.size
 end
 
 #def self.incomplete(num)
-#var = @todo[num]
-#@pending << var
-#@completed.delete var
+#var = $todo[num]
+#$pending << var
+#$completed.delete var
 #end
 
-def self.delete(num)
-@completed.delete_at(num - 1)
-return @completed.size
+def delete(num)
+$completed.delete_at(num - 1)
+return $completed.size
 end
-
-def self.modify(num,title)
-@pending[num - 1] = title
-return @pending[num - 1]
-end
-
-def self.empty
-@pending.clear
-@completed.clear
-@todo.clear
+def empty
+$pending.clear
+$completed.clear
+$todo.clear
 return true
 end
+def modify(num,title)
+$pending[num - 1] = title
 
-def self.show_pending(num)
- return @pending[num - 1]
+return $pending[num - 1]
 end
 
-def self.show_completed(num)
- return @completed[num - 1]
+def show_pending(num)
+ return $pending[num - 1]
+end
+
+def show_completed(num)
+ return $completed[num - 1]
 end
 
 end
-#t = Todolist.new('usha.txt')
+
+$s = Todolist.new("usha.txt")
 #t.pending
-#t.complete()
+#t.complete(num)
 #t.list
 #t.completed
-#t.delete
-#t.modify
-#t.add
+#t.delete(num)
+#t.modify(num,title)
+#t.add(item)
 
--- todonew/test/todo_test.rb
+++ todonew/test/todo_test.rb
@@ -1,66 +1,71 @@
 require_relative '../lib/todolist.rb'
 require 'test/unit'
-
-
-
 class TestTodo < Test::Unit::TestCase
- def test_empty
-  assert_equal true,Todolist.empty
-  assert_equal 0,Todolist.pending
-  assert_equal 0,Todolist.completed
-  assert_equal 0,Todolist.list
+ 
+  $s = Todolist.new("usha.txt")
+ 
+
+ def test_zempty
+  assert_equal true,$s.empty
+  assert_equal 0,$s.pending
+  assert_equal 0,$s.completed
+  assert_equal 0,$s.list
  end
   def test_add1
-   assert_equal 1,Todolist.add("open")
-   assert_equal 1,Todolist.list
-   assert_equal 1,Todolist.pending
-   assert_equal 0,Todolist.completed
+   
+   assert_equal 1,$s.add("open")
+   assert_equal 1,$s.list
+   assert_equal 1,$s.pending
+   assert_equal 0,$s.completed
  end
   def test_add2
-   assert_equal 2,Todolist.add("search")
-   assert_equal 2,Todolist.list
-   assert_equal 2,Todolist.pending
-   assert_equal 0,Todolist.completed
+   assert_equal 2,$s.add("search")
+   assert_equal 2,$s.list
+   assert_equal 2,$s.pending
+   assert_equal 0,$s.completed
  end
  def test_add3
-   assert_equal 3,Todolist.add("write")
-   assert_equal 3,Todolist.list
-   assert_equal 3,Todolist.pending
-   assert_equal 0,Todolist.completed
+
+   assert_equal 3,$s.add("write")
+   assert_equal 3,$s.list
+   assert_equal 3,$s.pending
+   assert_equal 0,$s.completed
  end
  def test_add4
-   assert_equal 4,Todolist.add("save")
-   assert_equal 4,Todolist.list
-   assert_equal 4,Todolist.pending
-   assert_equal 0,Todolist.completed
+   assert_equal 4,$s.add("save")
+   assert_equal 4,$s.list
+   assert_equal 4,$s.pending
+   assert_equal 0,$s.completed
  end
- 
+  
  def test_complete
-   assert_equal 1,Todolist.complete(1)
-   assert_equal 3,Todolist.pending
-   assert_equal 1,Todolist.completed
-   assert_equal 4,Todolist.list
+   assert_equal 1,$s.complete(1)
+   assert_equal 3,$s.pending
+   assert_equal 1,$s.completed
+   assert_equal 4,$s.list
+ end
+def test_delete
+   assert_equal 0,$s.delete(1)
+   assert_equal 3,$s.pending
+   assert_equal 0,$s.completed
+   assert_equal 3,$s.list
  end
 
  def test_modify
-   assert_equal "merge",Todolist.modify(2,"merge")
-   assert_equal "read",Todolist.modify(1,"read")
-   assert_equal 2,Todolist.pending
-   assert_equal 0,Todolist.completed
-   assert_equal 2,Todolist.list
+   assert_equal "merge",$s.modify(3,"merge")
+   assert_equal "read",$s.modify(4,"read")
+   assert_equal 4,$s.pending
+   assert_equal 0,$s.completed
+   assert_equal 4,$s.list
  end
  
- def test_delete
-   assert_equal 0,Todolist.delete(1)
-   assert_equal 3,Todolist.pending
-   assert_equal 0,Todolist.completed
-   assert_equal 3,Todolist.list
- end
-def test_show_pending
-  assert_equal "merge",Todolist.show_pending(2)
+
+def test_pend
+  assert_equal "write",$s.show_pending(2)
+  assert_equal "read",$s.show_pending(4)
 end
-def test_show_completed
-  assert_equal nil,Todolist.show_completed(1)
+def test_qcompleted
+  assert_equal nil,$s.show_completed(1)
   
 end
 end
