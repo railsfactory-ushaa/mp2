5707886fc9dff921f4e4760a3ab70f31722cf060
usha <ushaambig@railsfactory.org>
2014-07-11 15:36:05 +0530

    Passed 5 test with 21 assertions

--- todonew/lib/todolist.rb
+++ todonew/lib/todolist.rb
@@ -1,74 +1,72 @@
 class Todolist 
-$pending = []
-$todo = []
-$completed = []
- attr_accessor :filename
+# attr_accessor :filename
  def initialize(filename)
    @filename = filename
+
+@pending = []
+@todo = []
+@completed = []
+
+
+
  end
 
 def pending
-  return $pending.size
+  return @pending
 end
 
 def list
-  $todo = $pending + $completed
- return $todo.size
+  @todo = @pending + @completed
+ @todo
 end
 
 def completed
-   return $completed.size
+   @completed
 end
 
 def add(title)
-  $pending << title
- return $pending.count
+  @pending << title
+  @pending
 end
 
 def complete(num)
-$completed << $pending[num - 1]
-$pending.delete_at(num - 1)
-return $completed.size
+@completed << @pending[num - 1]
+@pending.delete_at(num - 1)
+@completed
 end
 
 #def self.incomplete(num)
-#var = $todo[num]
-#$pending << var
-#$completed.delete var
+#var = @todo[num]
+#@pending << var
+#@completed.delete var
 #end
 
 def delete(num)
-$completed.delete_at(num - 1)
-return $completed.size
+@completed.delete_at(num - 1)
+@completed
 end
+
 def empty
-$pending.clear
-$completed.clear
-$todo.clear
+@pending = []
+@completed = []
+@todo = []
 return true
 end
-def modify(num,title)
-$pending[num - 1] = title
 
-return $pending[num - 1]
+def modify(num,title)
+@pending[num - 1] = title
+
+return @pending[num - 1]
 end
 
 def show_pending(num)
- return $pending[num - 1]
+ return @pending[num - 1]
 end
 
 def show_completed(num)
- return $completed[num - 1]
+ return @completed[num - 1]
 end
 
 end
 
-$s = Todolist.new("usha.txt")
-#t.pending
-#t.complete(num)
-#t.list
-#t.completed
-#t.delete(num)
-#t.modify(num,title)
-#t.add(item)
 
--- todonew/test/todo_test.rb
+++ todonew/test/todo_test.rb
@@ -2,69 +2,127 @@ require_relative '../lib/todolist.rb'
 require 'test/unit'
 class TestTodo < Test::Unit::TestCase
  
-  $s = Todolist.new("usha.txt")
+
+def setup
+
+  @a = Todolist.new("usha.txt")
  
+end
+
+def teardown
+
+  @a=nil
+ 
+end
+
+
  def test_zempty
-  assert_equal true,$s.empty
-  assert_equal 0,$s.pending
-  assert_equal 0,$s.completed
-  assert_equal 0,$s.list
+  @a.empty
+  assert_equal 0,@a.pending.size
+  assert_equal 0,@a.completed.size
+  assert_equal 0,@a.list.size
  end
+
+
+
   def test_add1
-   
-   assert_equal 1,$s.add("open")
-   assert_equal 1,$s.list
-   assert_equal 1,$s.pending
-   assert_equal 0,$s.completed
+   @a.empty
+   @a.add("one")
+   assert_equal 1,@a.list.size
+   assert_equal 1,@a.pending.size
+   assert_equal 0,@a.completed.size
  end
+
+
+
   def test_add2
-   assert_equal 2,$s.add("search")
-   assert_equal 2,$s.list
-   assert_equal 2,$s.pending
-   assert_equal 0,$s.completed
+@a.empty
+@a.add("one")
+   @a.add("two")
+   assert_equal 2,@a.list.size
+   assert_equal 2,@a.pending.size
+   assert_equal 0,@a.completed.size
  end
+
+
+=begin
+
  def test_add3
 
-   assert_equal 3,$s.add("write")
-   assert_equal 3,$s.list
-   assert_equal 3,$s.pending
-   assert_equal 0,$s.completed
+   assert_equal 3,@a.add("write")
+   assert_equal 3,@a.list
+   assert_equal 3,@a.pending
+   assert_equal 0,@a.completed
  end
  def test_add4
-   assert_equal 4,$s.add("save")
-   assert_equal 4,$s.list
-   assert_equal 4,$s.pending
-   assert_equal 0,$s.completed
+   assert_equal 4,@a.add("save")
+   assert_equal 4,@a.list
+   assert_equal 4,@a.pending
+   assert_equal 0,@a.completed
  end
+=end
+
+
   
  def test_complete
-   assert_equal 1,$s.complete(1)
-   assert_equal 3,$s.pending
-   assert_equal 1,$s.completed
-   assert_equal 4,$s.list
- end
-def test_delete
-   assert_equal 0,$s.delete(1)
-   assert_equal 3,$s.pending
-   assert_equal 0,$s.completed
-   assert_equal 3,$s.list
+# precondition
+@a.empty
+@a.add("one")
+#before state
+assert_equal 1,@a.pending.size
+assert_equal 0,@a.completed.size
+assert_equal 1,@a.list.size
+
+
+#action
+   @a.complete(1)
+
+#after 
+assert_equal 0,@a.pending.size
+assert_equal 1,@a.completed.size
+assert_equal 1,@a.list.size
  end
 
+
+
+def test_delete
+# precondition
+@a.empty
+@a.add("one")
+ @a.complete(1)
+
+#before state
+#before state
+assert_equal 0,@a.pending.size
+assert_equal 1,@a.completed.size
+assert_equal 1,@a.list.size
+
+  @a.delete(1)
+
+   assert_equal 0,@a.pending.size
+   assert_equal 0,@a.completed.size
+   assert_equal 0,@a.list.size
+ end
+
+
+=begin
+
  def test_modify
-   assert_equal "merge",$s.modify(3,"merge")
-   assert_equal "read",$s.modify(4,"read")
-   assert_equal 4,$s.pending
-   assert_equal 0,$s.completed
-   assert_equal 4,$s.list
+   assert_equal "merge",@a.modify(3,"merge")
+   assert_equal "read",@a.modify(4,"read")
+   assert_equal 4,@a.pending
+   assert_equal 0,@a.completed
+   assert_equal 4,@a.list
  end
  
 
 def test_pend
-  assert_equal "write",$s.show_pending(2)
-  assert_equal "read",$s.show_pending(4)
+  assert_equal "write",@a.show_pending(2)
+  assert_equal "read",@a.show_pending(4)
 end
 def test_qcompleted
-  assert_equal nil,$s.show_completed(1)
+  assert_equal nil,@a.show_completed(1)
   
 end
+=end
 end
